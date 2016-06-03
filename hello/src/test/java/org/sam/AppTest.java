1e29509dc85d8aef78df636ecd9a1af48e184cfd
Meng Sun <sam_msn@126.com>
2016-06-04 02:11:13 +0800

    add hello module

---
+++ hello/src/test/java/org/sam/AppTest.java
@@ -0,0 +1,20 @@
+package org.sam;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
